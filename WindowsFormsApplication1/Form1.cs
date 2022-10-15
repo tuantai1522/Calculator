6a520cb237c2577d135af240b32b8e76807208ce
tuantai1511 <tai.bang157@gmail.com>
2022-10-15 14:16:39 +0700

    Add new event

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            textBox3.Text = int.Parse(textBox1.Text + textBox2.Text).ToString();
+            textBox3.Text = int.Parse(textBox1.Text - textBox2.Text).ToString();
         }
     }
 }
