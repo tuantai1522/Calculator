47498c6db741a1bf8d2894c52a091ddbc9322ebc
tuantai1511 <tai.bang157@gmail.com>
2022-10-15 14:03:41 +0700

    Update Form

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = int.Parse(textBox1.Text + textBox2.Text).ToString();
+        }
     }
 }
