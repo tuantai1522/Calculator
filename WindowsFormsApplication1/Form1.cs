53b917acb2d9ab5330e98171e50149593ddd1b79
tuantai1511 <tai.bang157@gmail.com>
2022-10-22 13:37:32 +0700

    Sua dieu kien nut cong

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -19,8 +19,10 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
-            textBox3.Text = int.Parse(textBox1.Text + textBox2.Text).ToString();
+            if (textBox1.Text != String.Empty)
+            {
+                textBox3.Text = int.Parse(textBox1.Text + textBox2.Text).ToString();
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
